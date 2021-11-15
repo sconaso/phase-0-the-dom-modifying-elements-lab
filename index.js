52be3f980499ddeb1215bfba2fdff22ff0917983
Scott Nason <scottmnason@gmail.com>
2021-11-14 18:37:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Scott is the champion";
+document.body.append(newHeader);
